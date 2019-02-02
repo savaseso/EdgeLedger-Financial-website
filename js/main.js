0e5ca809064ca190b9c96290c7b2cb0957497464
savaseso <rigbence@gmail.com>
2019-02-02 18:11:04 +0100

    fixing issues

--- js/main.js
+++ js/main.js
@@ -14,15 +14,15 @@
         800
         );
     }
-}); */
+});*/
 
 
 //navbar opacity when scrolling
-/* window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function() {
     if (window.scrollY > 150) {
       document.querySelector('#nav-bar').style.opacity = 0.9;
     }else{
       document.querySelector('#nav-bar').style.opacity = 1;
     }
   
-}); */
+}); 
